095c9d1090818d87708dc8aacf38312e6b5dc5f7
Jahid1999 <bsse1030@iit.du.ac.bd>
2022-09-10 00:01:33 +0600

    merge conflict test

--- test.cpp
+++ test.cpp
@@ -14,11 +14,24 @@ void swap(int a, int b)
 	
 }
 
+void printGreater(int a, int b){
+	if(a>b)
+		cout << a << " is greater" << endl;
+	else
+		cout << b << " is greater" << endl;
+}
+
 int main(void)
 {
+	int a=23, b=20;
+
 	int a=13, b=20;
 	
+	printGreater(a,b);
 	swap(a,b);
+
+	cout << "Git stash" << endl;
+	
 	
 	return 0;
 }
